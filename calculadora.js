a92f72dbcb05c5adb3b6e4629006d1aec52207f3
meandro02 <rd209422@gmail.com>
2024-06-20 22:02:08 -0600

    calculadora v2

--- calculadora.js
+++ calculadora.js
@@ -1,8 +1,3 @@
-/* *
- * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
- * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
- * session persistence, api calls, and more.
- * */
 const Alexa = require('ask-sdk-core');
 
 const LaunchRequestHandler = {
@@ -10,7 +5,7 @@ const LaunchRequestHandler = {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speakOutput = 'Hola  soy Alexa, bienvenido a mi calculadora uthh!';
+        const speakOutput = 'Hola, soy Alexa, bienvenido a mi calculadora u t h h. ¿Qué operación te gustaría realizar?';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -25,47 +20,55 @@ const calcularManejador = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'calcular';
     },
     handle(handlerInput) {
-        var operacion = handlerInput.requestEnvelope.request.intent.slots.operacion.value;
-        var primer_numero = Number(handlerInput.requestEnvelope.request.intent.slots.primer_numero.value);
-        var segundo_numero = Number(handlerInput.requestEnvelope.request.intent.slots.segundo_numero.value);
-        var resultado = 0;
-        var tipo_operacion = '';
-        
-        
-    
-        if (operacion === 'multiplicar')
-        {
-            tipo_operacion='multiplicacion'
-            resultado = primer_numero * segundo_numero;
+        const operacion = handlerInput.requestEnvelope.request.intent.slots.operacion.value;
+        const primer_numero = Number(handlerInput.requestEnvelope.request.intent.slots.primer_numero.value);
+        const segundo_numero = handlerInput.requestEnvelope.request.intent.slots.segundo_numero ? Number(handlerInput.requestEnvelope.request.intent.slots.segundo_numero.value) : null;
+        let resultado = 0;
+        let tipo_operacion = '';
+
+        switch (operacion) {
+            case 'multiplicar':
+                tipo_operacion = 'multiplicación';
+                resultado = primer_numero * segundo_numero;
+                break;
+            case 'dividir':
+                tipo_operacion = 'división';
+                resultado = primer_numero / segundo_numero;
+                break;
+            case 'restar':
+                tipo_operacion = 'resta';
+                resultado = primer_numero - segundo_numero;
+                break;
+            case 'sumar':
+                tipo_operacion = 'suma';
+                resultado = primer_numero + segundo_numero;
+                break;
+            case 'raiz':
+                tipo_operacion = 'raíz cuadrada';
+                resultado = Math.sqrt(primer_numero);
+                break;
+            case 'raíz':
+                tipo_operacion = 'raíz cuadrada';
+                resultado = Math.sqrt(primer_numero);
+                break;
+            case 'seno':
+                tipo_operacion = 'seno';
+                resultado = Math.sin(primer_numero);
+                break;
+            default:
+                tipo_operacion = 'desconocida';
+                resultado = 'Operación no soportada';
         }
-        
-        if (operacion === 'dividir')
-        {
-              tipo_operacion='divicion'
-            resultado = primer_numero / segundo_numero;
-        }
-        
-        if (operacion === 'restar')
-        {
-              tipo_operacion='resta'
-            resultado = primer_numero - segundo_numero;
-        }
-        
-        if (operacion === 'sumar')
-        {
-              tipo_operacion='suma'
-            resultado = primer_numero + segundo_numero;
-        }
-        
-        
-        const speakOutput = 'El resultado  de la' + tipo_operacion + "es " + resultado;
+
+        const speakOutput = `calculadora u t h h El resultado de la ${tipo_operacion} es ${resultado}`;
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
-             .reprompt(speakOutput)
+            .reprompt(speakOutput)
             .getResponse();
     }
 };
+
 const HelloWorldIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
@@ -76,7 +79,6 @@ const HelloWorldIntentHandler = {
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
 };
@@ -87,7 +89,7 @@ const HelpIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
     },
     handle(handlerInput) {
-      const speakOutput = 'Puedes pedirme que realice operaciones matemáticas como sumar, restar, multiplicar o dividir. ¿Qué operación te gustaría realizar?';
+        const speakOutput = 'Puedes pedirme que realice operaciones matemáticas como sumar, restar, multiplicar, dividir, calcular la raíz cuadrada o el seno. ¿Qué operación te gustaría realizar?';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -103,25 +105,21 @@ const CancelAndStopIntentHandler = {
                 || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
     },
     handle(handlerInput) {
-        const speakOutput = 'Goodbye!';
+        const speakOutput = '¡Adiós!';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .getResponse();
     }
 };
-/* *
- * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
- * It must also be defined in the language model (if the locale supports it)
- * This handler can be safely added but will be ingnored in locales that do not support it yet 
- * */
+
 const FallbackIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'Sorry, I don\'t know about that. Please try again.';
+        const speakOutput = 'Lo siento, no sé sobre eso. Por favor intenta nuevamente.';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -129,51 +127,37 @@ const FallbackIntentHandler = {
             .getResponse();
     }
 };
-/* *
- * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
- * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
- * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
- * */
+
 const SessionEndedRequestHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
     },
     handle(handlerInput) {
         console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
-        // Any cleanup logic goes here.
-        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+        return handlerInput.responseBuilder.getResponse();
     }
 };
-/* *
- * The intent reflector is used for interaction model testing and debugging.
- * It will simply repeat the intent the user said. You can create custom handlers for your intents 
- * by defining them above, then also adding them to the request handler chain below 
- * */
+
 const IntentReflectorHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
     },
     handle(handlerInput) {
         const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
-        const speakOutput = `You just triggered ${intentName}`;
+        const speakOutput = `Acabas de activar ${intentName}`;
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
 };
-/**
- * Generic error handling to capture any syntax or routing errors. If you receive an error
- * stating the request handler chain is not found, you have not implemented a handler for
- * the intent being invoked or included it in the skill builder below 
- * */
+
 const ErrorHandler = {
     canHandle() {
         return true;
     },
     handle(handlerInput, error) {
-        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
+        const speakOutput = 'Lo siento, tuve problemas para hacer lo que pediste. Por favor intenta nuevamente.';
         console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
 
         return handlerInput.responseBuilder
@@ -183,11 +167,6 @@ const ErrorHandler = {
     }
 };
 
-/**
- * This handler acts as the entry point for your skill, routing all request and response
- * payloads to the handlers above. Make sure any new handlers or interceptors you've
- * defined are included below. The order matters - they're processed top to bottom 
- * */
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
